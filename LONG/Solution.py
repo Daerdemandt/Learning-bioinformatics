092ec8b9c2805c45cd816df4f30fedb689d0ba96
Daerdemandt <daerdemandt@gmail.com>
2015-04-24 00:29:36 +0300

    Shortest superstring problem solution

--- LONG/Solution.py
+++ LONG/Solution.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+from functools import reduce
 # Returns all DNAs as {name:string}
 def read_DNAs_FASTA(input_file):
 	result = {}
@@ -17,77 +17,61 @@ def read_DNAs_FASTA(input_file):
 		result[name] = DNAstring
 	return result
 
-def make_tiling_table(strings):
-	length = len(strings)
-	tiling_table = []
-	def get_tiling_length(s1, s2):
-		for offset in range(len(s1)):
-			for j in range(min(len(s1) - offset, len(s2))):
-				if s1[offset + j] != s2[j]:
-					break
-			else:
-				return j
-		else:
-			return 0
-
-	for i in range(length):
-		tmp = []
-#		tmp = {}
-		for j in range(length):
-			tmp.append(None if i == j else get_tiling_length(strings[i], strings[j]))
-		tiling_table.append(tmp)
-	return tiling_table
-
-def get_shortest_superstring_recipe(tiling_table):
-	def compose(head, pieces):
-#		print("C:", head, pieces)
-		score, recipe = 0, []
-		if len(pieces) == 0:
-			return 0, [head]
-		pieces_weights = map(lambda p:(p, tiling_table[head][p-1]), pieces)# p-1 because of a trick in launching, see further
-		pieces_weights = sorted(pieces_weights, key=lambda pw: pw[1])
-		i = 0
-		for piece, weight in pieces_weights:
-			tmp_pieces = pieces[:]
-			tmp_pieces.remove(piece)
-			tmp_score, tmp_recipe = compose(piece, tmp_pieces)
-			if tmp_score + weight > score:
-				score = tmp_score + weight
-				recipe = tmp_recipe
-#!!!
-			i += 1
-			if i > 2:
+def get_tiling_length(s1, s2):
+	for offset in range(len(s1)):
+		for j in range(min(len(s1) - offset, len(s2))):
+			if s1[offset + j] != s2[j]:
 				break
-#!!!
-		return score, [head] + recipe
+		else:
+			return min(len(s1) - offset, len(s2))
+	else:
+		return 0
 
-	tiling_table.insert(0, [0 for i in range(len(tiling_table))]) # Eases launch
-	score, recipe = compose(0, list(range(1, len(tiling_table))))
-	del tiling_table[0]
-	recipe.remove(0)
-	return list(map(lambda x: x-1, recipe))
+def overlap_cat(s1, s2):
+	offset = get_tiling_length(s1,s2)
+	return s1 + s2[offset:]
 
-def construct_string(strings, tiling_table, recipe):
-	string = '' + strings[recipe[0]]
-	previous_piece = recipe[0]
-	for piece in recipe[1:]:
-		offset = tiling_table[previous_piece][piece]
-		string += strings[piece][offset + 1:]
-		previous_piece = piece
-	return string
+
+def sure_reduction(strings):
+	number_of_strings = len(strings)
+
+	def get_sure_things():
+		sure_things = {}
+		sure_length = len(strings[0]) // 2 # Heuristic that is said to work on our input
+		for i in range(number_of_strings):
+			for j in range(number_of_strings):
+				if i == j:
+					continue
+				if get_tiling_length(strings[i], strings[j]) > sure_length:
+					sure_things[i] = j
+
+		return sure_things
+
+	def get_contigs(sure_things):
+		contigs = []
+		tails = set(sure_things.values())
+		contig_heads = set(range(number_of_strings)) - tails # A string is either a start of contig or a continuation of it
+		for head in contig_heads:
+			cur = head
+			contig = [head]
+			while cur in sure_things:
+				contig.append(sure_things[cur])
+				cur = sure_things[cur]
+			contigs.append(contig)
+		return contigs
+
+	def contig_reduction(contigs):
+		result = []
+		for contig in contigs:
+			result.append(reduce(overlap_cat, map(lambda i: strings[i], contig)))
+		return result
+	return contig_reduction(get_contigs(get_sure_things()))
+# end of sure_reduction
 
 def main():
 	with open("Input.fasta") as input_file:
-		print("Reading input file...")
 		strings = list(read_DNAs_FASTA(input_file).values())
-		print("Making tiling_table...")
-		tiling_table = make_tiling_table(strings)
-		print(tiling_table)
-#		return
-		print("Getting recipe...")
-		recipe = get_shortest_superstring_recipe(tiling_table)
-		print(recipe)
 		with open("Output.txt", "w") as output_file:
-			print(construct_string(strings, tiling_table, recipe), file=output_file)
+			print(sure_reduction(strings)[0], file=output_file)# there's actually just one big contig in input data
 
 main()
